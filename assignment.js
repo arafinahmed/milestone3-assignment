be3ce8c087031fa4c8e9a97ff694d8c7bd7aeebf
Arafin Ahmed <arafin2021@gmail.com>
2021-01-22 01:35:22 +0600

    done

--- assignment.js
+++ assignment.js
@@ -1,8 +1,8 @@
-//https://github.com/arafinahmed/milestone3-assignment
+// https://github.com/arafinahmed/milestone3-assignment
 
 
-//A function that will convert kilometer to meter
-//we know that 1 kilometer = 1000 meter
+// A function that will convert kilometer to meter
+// we know that 1 kilometer = 1000 meter
 function kilometerToMeter(kilometer) {
     if (typeof kilometer == "number") {
         if (kilometer >= 0) {
@@ -14,28 +14,28 @@ function kilometerToMeter(kilometer) {
 }
 
 
-//Watch Price = 50, phone price = 100 and laptop price = 500
-//we will calculate the total cost of the three product
-//input is the numbers of watch, phone and laptop
+// Watch Price = 50, phone price = 100 and laptop price = 500
+// we will calculate the total cost of the three product
+// input is the numbers of watch, phone and laptop
 function budgetCalculator(watch, phone, laptop) {
     if (typeof watch == "number" && typeof phone == "number" && typeof laptop == "number") {
-        if (watch >= 0 && phone >= 0 && laptop >= 0) {
+        if (watch >= 0 && phone >= 0 && laptop >= 0 && Number.isInteger(watch) && Number.isInteger(phone) && Number.isInteger(laptop)) {
             var cost = watch * 50 + phone * 100 + laptop * 500;
             return cost;
         }
-        return "Item can not be negative";
+        return "Item can not be negative or fraction";
     }
     return "Invalid input";
 }
 
 
-//Hotel cost day 1 to day 10 -> 100 per day
-//Hotel cost day 11 to day 20 -> 80 per day
-//Hotel cost from day 21 -> 50 per day
-//input is the numbers of day
+// Hotel cost day 1 to day 10 -> 100 per day
+// Hotel cost day 11 to day 20 -> 80 per day
+// Hotel cost from day 21 -> 50 per day
+// input is the numbers of day
 function hotelCost(day) {
     if (typeof day == "number") {
-        if (day > 0) {
+        if (day > 0 && Number.isInteger(day)) {
             var firstTen = day;
             var secondTen = 0;
             var extraDay = 0;
@@ -52,14 +52,15 @@ function hotelCost(day) {
             var totalCost = firstTen * 100 + secondTen * 80 + extraDay * 50;
             return totalCost;
         }
-        return "Day can not be zero or negative";
+        return "Day can not be zero or negative or fraction";
     }
     return "Invalid Input";
 
 }
 
+//Find the largest name in a array
 function megaFriend(friend){
-    if(typeof friend == "object"){
+    if(Array.isArray(friend)){
         if(friend.length == 0){
             return "Array is empty";
         }
@@ -76,12 +77,3 @@ function megaFriend(friend){
     return "Input is not an array";
 }
 
-console.log(kilometerToMeter())
-console.log(hotelCost(-20))
-var arafin = "ahmed";
-console.log(typeof 12.8)
-console.log(arafin[0])
-
-console.log(typeof [0, "a", 2]);
-console.log(megaFriend([]));
-console.log(budgetCalculator(5,-1,1));
